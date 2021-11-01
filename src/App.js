246ab420c18cab7810c0e20089dff2bf21815d2b
elcoruco <elcoruco@gmail.com>
2021-10-31 23:07:17 -0600

    add basic tble for items

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {csv} from "d3-fetch";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState(null);
+  csv("vestuarios.csv").then(res => {
+    if(!items){
+      setItems(res.reverse());
+    }
+  });
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="table-auto border-collapse border border-grey">
+        <thead>
+          <tr>
+            <th className="border border-grey-600">Imagen</th>
+            <th className="border border-grey-600">Artista</th>
+            <th className="border border-grey-600">Tipo</th>
+            <th className="border border-grey-600">Color</th>
+            <th className="border border-grey-600">Evento</th>
+            <th className="border border-grey-600">Año</th>
+            <th className="border border-grey-600">País</th>
+            <th className="border border-grey-600">Estado</th>
+            <th className="border border-grey-600">Canción</th>
+            <th className="border border-grey-600">referencia</th>
+            <th className="border border-grey-600">facebook</th>
+            <th className="border border-grey-600">instagram</th>
+          </tr>
+        </thead>
+        <tbody>
+        {items && items.map( (item,i) => 
+          <tr key={`item-${i}`}>
+            <td className="border border-grey-600"><img style={{
+              'width': '250px',
+              'borderRadius': '8px',
+              'padding' : '.2em'
+              }} src={`img/covers/${item.pic}.jpg`}></img></td>
+            <td className="border border-grey-600">{item.source}</td>
+            <td className="border border-grey-600">{item.type}</td>
+            <td className="border border-grey-600">{item.color}</td>
+            <td className="border border-grey-600">{item.event}</td>
+            <td className="border border-grey-600">{item.year}</td>
+            <td className="border border-grey-600">{item.country}</td>
+            <td className="border border-grey-600">{item.state}</td>
+            <td className="border border-grey-600">{item.song}</td>
+            <td className="border border-grey-600"><a href={item.url} target="_blank">referencia</a></td>
+            <td className="border border-grey-600">{item.fb && <a href={item.fb}>link</a>}</td>
+            <td className="border border-grey-600">{item.instagram && <a href={item.instagram}>link</a>}</td>
+          </tr>
+        )}
+        </tbody>
+      </table>
     </div>
   );
 }
