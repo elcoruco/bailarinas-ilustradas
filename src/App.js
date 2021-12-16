238eed9d77b3ea152d1e60348efdfd12bbba542d
elcoruco <elcoruco@gmail.com>
2021-12-15 22:29:40 -0600

    update table

--- src/App.js
+++ src/App.js
@@ -3,6 +3,44 @@ import { useState } from "react";
 
 function App() {
   const [items, setItems] = useState(null);
+  const tags = [ "vestuariosilustrados", "salsacostume", "fashionillustration", 
+    "procreateFashionIllustration", "fashionDesign", "illustration", "animeDancer", 
+    "animeCostume", "procreateApp", "Salsa"];
+  
+  const makeHash = text => text ? text.replaceAll(" ", "") : '';
+  
+  const makeInstagramText = item => {
+    return `El siguiente post es un vestuario de ${item.source} ${item.country ? '(' + item.country + ')' : ''}:
+    Se presentó en el #${makeHash(item.event)} ${item.year}
+    referencia: ${item.url}
+    #${makeHash(item.tipo)}
+    #${item.color}
+    #${tags.join(' #')}
+    ${item.fb ? 'en fb: ' + item.fb : ''}
+    ${item.song ? 'canción: ' + item.song : ''}`
+    
+  }
+  csv("vestuarios.csv").then(res => {
+    if(!items){
+      setItems(res.reverse());
+    }
+  });
+
+  const makeFacebookText = item => {
+    return <p>Ilustración basada en un vestuario de {item.source} {item.country ? '(' + item.country + ')' : ''}:
+    <br></br>
+    presentado en el {item.event} {item.year}
+    <br></br>
+    referencia: {item.url}
+    <br></br>
+    #{makeHash(item.tipo)}
+    #{item.color}
+    {tags.join(' #')}
+    <br></br>
+    {item.fb ? 'en fb: ' + item.fb : ''}
+    <br></br>
+    {item.song ? 'canción: ' + item.song : ''}</p>;
+  }
   csv("vestuarios.csv").then(res => {
     if(!items){
       setItems(res.reverse());
@@ -26,6 +64,8 @@ function App() {
             <th className="border border-grey-600">referencia</th>
             <th className="border border-grey-600">facebook</th>
             <th className="border border-grey-600">instagram</th>
+            <th className="border border-grey-600">instagram TEXT</th>
+            <th className="border border-grey-600">facebook TEXT</th>
           </tr>
         </thead>
         <tbody>
@@ -47,6 +87,8 @@ function App() {
             <td className="border border-grey-600"><a href={item.url} target="_blank">referencia</a></td>
             <td className="border border-grey-600">{item.fb && <a href={item.fb}>link</a>}</td>
             <td className="border border-grey-600">{item.instagram && <a href={item.instagram}>link</a>}</td>
+            <td className="border border-grey-600">{makeInstagramText(item)}</td>
+            <td className="border border-grey-600">{makeFacebookText(item)}</td>
           </tr>
         )}
         </tbody>
